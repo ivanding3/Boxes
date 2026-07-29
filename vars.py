5a69dcfea68c23a680a25feff030366d59dbd531
ivanding3 <ivanding3@gmail.com>
2026-07-28 22:00:35 -0700

    Fixed scaling issue based on user resolution

--- vars.py
+++ vars.py
@@ -3,7 +3,7 @@ import pygame
 menu_open = False
 game_running = True
 pygame.display.init()
-resolution = pygame.display.get_desktop_sizes()[0]
+resolution = (1920,1080)
 (screen_width,screen_height) = resolution
 screen = pygame.display.set_mode((resolution),pygame.SCALED,vsync=1,)
 dt = 0
